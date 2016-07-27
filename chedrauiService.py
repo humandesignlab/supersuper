c9f0f5982747220a621c05989bceba421438ce53
Humberto Mangino <hmangino@gmail.com>
2016-07-27 18:56:05 -0500

    Make chedrauiService.py a function

--- chedrauiService.py
+++ chedrauiService.py
@@ -10,46 +10,49 @@ import pandas as pd
 
 #chedrauiUrl = 'http://www.chedraui.com.mx/index.php/interlomas/catalogsearch/result/?cat=0&q='
 
-searchTerm = 'jamón serrano'
-searchQuery = searchTerm.replace(" ", "+")
-productNames = []
-allPrices = []
-cj = CookieJar()
+def searchService(searchString):
 
-preOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-prep = preOpener.open('http://www.chedraui.com.mx/index.php/interlomas/catalogsearch/result/?cat=0&p=1&q=' + searchQuery)
-preSoup = BeautifulSoup(prep, "lxml")
-pagerTotal = preSoup.find('div', class_='pager')
-children = pagerTotal.find_all("li")
+	searchTerm = searchString
+	searchQuery = searchTerm.replace(" ", "+")
+	productNames = []
+	allPrices = []
+	cj = CookieJar()
 
-if len(children) == 0:
-	pages = len(children) +2
-else:
-	pages = len(children)
+	preOpener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+	prep = preOpener.open('http://www.chedraui.com.mx/index.php/interlomas/catalogsearch/result/?cat=0&p=1&q=' + searchQuery)
+	preSoup = BeautifulSoup(prep, "lxml")
+	pagerTotal = preSoup.find('div', class_='pager')
+	children = pagerTotal.find_all("li")
 
-#print pages
-for page in range(1,pages):
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-	p = opener.open('http://www.chedraui.com.mx/index.php/interlomas/catalogsearch/result/?cat=0&p=' + str(page) + '&q=' + searchQuery)
-	mySoup = BeautifulSoup(p, "lxml")
+	if len(children) == 0:
+		pages = len(children) +2
+	else:
+		pages = len(children)
 
-	chedraui_products = mySoup.find_all('div', class_='f-fix')
-	for name in chedraui_products:
-		names = name.find_all('a', class_='name-product')
-		productNames.append(names[0].find(text=True).strip())
+	#print pages
+	for page in range(1,pages):
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+		p = opener.open('http://www.chedraui.com.mx/index.php/interlomas/catalogsearch/result/?cat=0&p=' + str(page) + '&q=' + searchQuery)
+		mySoup = BeautifulSoup(p, "lxml")
 
-	chedraui_prices = mySoup.find_all('div', class_='price-box')
-	for price in chedraui_prices:
-		prices = price.find_all('span', class_='price')
-		allPrices.append(prices[-1].find(text=True).strip())
+		chedraui_products = mySoup.find_all('div', class_='f-fix')
+		for name in chedraui_products:
+			names = name.find_all('a', class_='name-product')
+			productNames.append(names[0].find(text=True).strip())
 
-	print "Retreived data from page: " + str(page)
+		chedraui_prices = mySoup.find_all('div', class_='price-box')
+		for price in chedraui_prices:
+			prices = price.find_all('span', class_='price')
+			allPrices.append(prices[-1].find(text=True).strip())
 
-print "Total de productos: ", len(productNames)
-print "Total de precios: ", len(allPrices)
+		print "Retreived data from page: " + str(page)
 
-df = pd.DataFrame(productNames, columns=['Producto'])
-df['Precio']=allPrices
+	print "Total de productos: ", len(productNames)
+	print "Total de precios: ", len(allPrices)
 
-print df
-df.to_csv('searches/outchedraui.csv', encoding='utf-8')
+	df = pd.DataFrame(productNames, columns=['Producto'])
+	df['Precio']=allPrices
+	df.to_csv('searches/outchedraui.csv', encoding='utf-8')
+	print df
+
+searchService('jabon zote')
---
+++ main.py
@@ -0,0 +1,3 @@
+import chedrauiService
+
+chedrauiService()
