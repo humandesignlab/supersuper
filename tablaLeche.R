469bf8bf9df7e5691ef772bb9da2414348f0383e
Humberto Mangino <hmangino@gmail.com>
2016-08-05 14:33:20 -0500

    Deleted: chedrauiService.pyc, main.py, tablaLecha.R, test.py

--- tablaLeche.R
+++
@@ -1,7 +0,0 @@
-library(jsonlite)
-library(data.table)
-jsonDataLecheSuperama <- fromJSON("http://www.superama.com.mx/buscador/resultado?busqueda=&departamento=d-lacteos-y-huevo&familia=f-leche&linea=l-entera")
-itemLeche <- jsonDataLecheSuperama$Products$Description
-precioLecheEntera <- jsonDataLecheSuperama$Products$PrecioNumerico
-lecheSuperamaDF <- data.frame(itemLeche, precioLecheEntera)
-lecheSuperamaDF
--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/local/bin/python
-# -*- coding: utf-8 -*-
-
-
-from HTMLParser import HTMLParser
-pars = HTMLParser()
-test = pars.unescape('&copy; &euro; &#225;')
-print test.encode('utf-8')
